f9cf1cee22d31ec7012560e8ea6b29ec0001b956
Paula Sousa <paulasousa137@gmail.com>
2022-08-06 11:03:00 +0200

    WizardFinal

--- src/characters/Wizard.java
+++ src/characters/Wizard.java
@@ -1,7 +1,29 @@
 package characters;
 
-public class Wizard {
+public class Wizard extends Character {
     private int mana;
     private int intelligence;
 
+    public Wizard(int id, String name, int hp, boolean isAlive, int mana, int intelligence) {
+        super(id, name, hp, isAlive);
+        this.mana = mana;
+        this.intelligence = intelligence;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+}
 }

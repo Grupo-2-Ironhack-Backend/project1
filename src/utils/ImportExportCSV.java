cc8fc803eadd4b2db1bb205b91ae59c7aefb8c71
Alex Dopico <alex.dopico@gmail.com>
2022-08-06 19:11:39 +0200

    created importCSVParty that returns an ArrayList of members to a party

--- src/utils/ImportExportCSV.java
+++ src/utils/ImportExportCSV.java
@@ -3,6 +3,7 @@ package utils;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ImportExportCSV {
     public static void exportPartyToCSV(ArrayList<String> party) {
@@ -15,5 +16,20 @@ public class ImportExportCSV {
         } catch (Exception e) {
             System.out.println("La banda ha desaparecido en un vórtice de malignidad. No hay banda.");
         }
+    }
+
+    public static ArrayList<String> importCSVParty() {
+        ArrayList<String> party = new ArrayList<>();
+
+        try (Scanner scanFile = new Scanner(new File("./party.csv"))) {
+            while (scanFile.hasNextLine()) {
+                String dato = scanFile.nextLine();
+                party.add(dato);
+            }
+        } catch (Exception e) {
+            System.out.println("La banda no ha podido llegar a la batalla. Estan todos MUERTOS.");
+        }
+
+        return party;
     }
 }

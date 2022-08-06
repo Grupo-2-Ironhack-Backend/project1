b644cf501c423e1063e25e846123c41e9425c6bf
gerardVM <valverdegerard@gmail.com>
2022-08-06 09:19:27 +0200

    Warrior class completed

--- src/characters/Warrior.java
+++ src/characters/Warrior.java
@@ -1,4 +1,28 @@
 package characters;
 
-public class Warrior {
+public class Warrior extends Character {
+
+    private int stamina;
+    private int strength;
+    public Warrior(int id, String name, int hp, boolean isAlive, int stamina, int strength) {
+        super(id, name, hp, isAlive);
+        this.stamina = stamina;
+        this.strength = strength;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 }

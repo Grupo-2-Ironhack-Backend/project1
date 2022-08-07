941d528edef099283440463fbdafb0de5a0d5126
Alex Dopico <alex.dopico@gmail.com>
2022-08-07 20:01:54 +0200

    created ConsoleColor and TypeOfMessages that categorizes every kind of message the program can generate. Each type of message has a color associated in the Enum ConsoleColors.

---
+++ src/utils/TypeOfMessages.java
@@ -0,0 +1,12 @@
+package utils;
+
+public enum TypeOfMessages {
+    CREATION,
+    ATTACK,
+    DAMAGE_DEALT,
+    DAMAGE_TAKEN,
+    PARTY_JOINED,
+    PARTY_CREATED,
+    DEATH,
+    DISSAPEARED;
+}
